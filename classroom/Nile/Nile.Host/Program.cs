865ac063e7c3bd4d60825750009b419536ecee13
kevin-belknap <kevin.belknap86099@my.tccd.edu>
2017-09-11 18:16:33 -0500

    Created Methods

--- classroom/Nile/Nile.Host/Program.cs
+++ classroom/Nile/Nile.Host/Program.cs
@@ -10,29 +10,61 @@ namespace Nile.Host
     {
         static void Main( string[] args )
         {
-            Console.WriteLine("A)dd Product");
-            Console.WriteLine("L)List Products");
-            Console.WriteLine("Q)uit");
+            bool quit = false;
+            do
+            { 
+                char choice = GetInput();
 
-            string input = Console.ReadLine();
+                switch (choice)
+                {
+                    //case statements (more efficient then else if. Use when finite number of statements)
+                    //[case][label]:statement;
+                    //[default:statement;
 
-            if (input == "A")
-            {
-                //do something
-            }
-            else if (input == "L")
-            {
+                    //every case statement has to have a break to prevent fall through. No blocks needed if single statement.  C# by default does not allow fall through.  You can
+                    //do fall through by having a same case with nothing being it (look at L below).
+                    case 'A': AddProduct(); break;
 
-            }
-            else if (input == "Q")
-            {
+                    case 'l':    //allowing fall through
+                    case 'L': ListProducts(); break;
+                    case 'Q': quit = true; break;
+                }    
+            } while (!quit);
+        }
 
-            }
-            else
-            {
-                //error
-            }
+        private static void ListProducts()
+        {
+            throw new NotImplementedException();
+        }
 
+        private static void AddProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        static char GetInput()
+        {
+            while (true)
+            { 
+                Console.WriteLine("A)dd Product");
+                Console.WriteLine("L)List Products");
+                Console.WriteLine("Q)uit");
+
+                string input = Console.ReadLine();
+
+
+                char letter = Char.ToUpper(input[0]);
+
+                if (letter == 'A')
+                    return 'A';
+                else if (letter == 'L')
+                    return 'L';
+                else if (letter == 'Q')
+                    return 'Q';
+
+                //Error
+                Console.WriteLine("Please choose a valid option");
+            };
         }
 
         static void Main2( string[] args)

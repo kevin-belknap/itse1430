b28b058061e1a40d0033ee7343ecf5c0a7046687
kevin-belknap <kevin.belknap86099@my.tccd.edu>
2017-11-29 18:26:59 -0600

    Added Views

--- classroom/Section 5/Nile.Web/Controllers/HomeController.cs
+++ classroom/Section 5/Nile.Web/Controllers/HomeController.cs
@@ -17,12 +17,5 @@ namespace Nile.Web.Controllers {
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
